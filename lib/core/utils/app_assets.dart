69ad75fad04f4cc68386c642e316ac1de6051e5e
Fatma Alaa <fatmalaa2002@gmail.com>
2024-12-22 03:45:04 +0200

    assets_Update

--- lib/core/utils/app_assets.dart
+++ lib/core/utils/app_assets.dart
@@ -1,4 +1,16 @@
 class AppAssets{
   static const String basePath= 'assets/images/';
   static const String firstPhoto='${basePath}photo.jpg';
+  static const String search='${basePath}Search.png';
+  static const String star='${basePath}star.png';
+  static const String map='${basePath}mapIcon.png';
+  static const String arrow1='${basePath}arrow1.png';
+  static const String arrow2='${basePath}arrow2.png';
+  static const String award='${basePath}award.png';
+  static const String filter='${basePath}Filter.png';
+  static const String heart='${basePath}heart.png';
+  static const String locationIcon='${basePath}locationIcon.png';
+  static const String sun='${basePath}sun-bed.png';
+
+
 }
